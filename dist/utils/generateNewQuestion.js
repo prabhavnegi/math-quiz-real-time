0c38f4cfe076c4260f9c2c5f5c6cf6b56ecc61d4
prabhavnegi <prabhav511@gmail.com>
2024-10-03 17:48:00 +0530

    change

--- dist/utils/generateNewQuestion.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateNewQuestion = void 0;
-const mathProblems_1 = require("./mathProblems");
-const generateNewQuestion = () => {
-    const { question, answer } = mathProblems_1.MathProblemGenerator.generate();
-    return { question, answer };
-};
-exports.generateNewQuestion = generateNewQuestion;
